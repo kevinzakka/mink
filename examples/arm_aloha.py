f3a47702c2907edb5ee6c2f8538cf7408a09148a
Kevin Zakka <kevinarmandzakka@gmail.com>
2025-02-25 22:38:29 -0800

    Small fix to the exit criterion.

--- examples/arm_aloha.py
+++ examples/arm_aloha.py
@@ -157,7 +157,7 @@ if __name__ == "__main__":
                 l_err = l_ee_task.compute_error(configuration)
                 l_pos_achieved = np.linalg.norm(l_err[:3]) <= pos_threshold
                 l_ori_achieved = np.linalg.norm(l_err[3:]) <= ori_threshold
-                r_err = l_ee_task.compute_error(configuration)
+                r_err = r_ee_task.compute_error(configuration)
                 r_pos_achieved = np.linalg.norm(r_err[:3]) <= pos_threshold
                 r_ori_achieved = np.linalg.norm(r_err[3:]) <= ori_threshold
                 if (
