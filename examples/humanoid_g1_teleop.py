f29a56775ebd615c9834c9de5ddba37c7c5c2f72
Kevin Zakka <kevinarmandzakka@gmail.com>
2025-03-02 14:42:18 -0800

    Teleop example with G1.

--- examples/humanoid_g1_teleop.py
+++ examples/humanoid_g1_teleop.py
@@ -7,14 +7,13 @@ from loop_rate_limiters import RateLimiter
 import mink
 
 _HERE = Path(__file__).parent
-_XML = _HERE / "unitree_g1" / "scene.xml"
+_XML = _HERE / "unitree_g1" / "scene_table.xml"
 
 
 if __name__ == "__main__":
     model = mujoco.MjModel.from_xml_path(_XML.as_posix())
 
     configuration = mink.Configuration(model)
-
     feet = ["right_foot", "left_foot"]
     hands = ["right_palm", "left_palm"]
 
@@ -23,18 +22,18 @@ if __name__ == "__main__":
             frame_name="pelvis",
             frame_type="body",
             position_cost=0.0,
-            orientation_cost=10.0,
+            orientation_cost=1.0,
             lm_damping=1.0,
         ),
         torso_orientation_task := mink.FrameTask(
             frame_name="torso_link",
             frame_type="body",
             position_cost=0.0,
-            orientation_cost=10.0,
+            orientation_cost=1.0,
             lm_damping=1.0,
         ),
-        posture_task := mink.PostureTask(model, cost=1.0),
-        com_task := mink.ComTask(cost=50.0),
+        posture_task := mink.PostureTask(model, cost=1e-1),
+        com_task := mink.ComTask(cost=10.0),
     ]
 
     feet_tasks = []
@@ -42,8 +41,8 @@ if __name__ == "__main__":
         task = mink.FrameTask(
             frame_name=foot,
             frame_type="site",
-            position_cost=200.0,
-            orientation_cost=10.0,
+            position_cost=10.0,
+            orientation_cost=1.0,
             lm_damping=1.0,
         )
         feet_tasks.append(task)
@@ -54,12 +53,32 @@ if __name__ == "__main__":
         task = mink.FrameTask(
             frame_name=hand,
             frame_type="site",
-            position_cost=200.0,
-            orientation_cost=10.0,
+            position_cost=5.0,
+            orientation_cost=1.0,
             lm_damping=1.0,
         )
         hand_tasks.append(task)
     tasks.extend(hand_tasks)
+
+    # Enable collision avoidance between the following geoms.
+    # left hand - table, right hand - table
+    # left hand - left thigh, right hand - right thigh
+    collision_pairs = [
+        (["left_hand_collision", "right_hand_collision"], ["table"]),
+        (["left_hand_collision"], ["left_thigh"]),
+        (["right_hand_collision"], ["right_thigh"]),
+    ]
+    collision_avoidance_limit = mink.CollisionAvoidanceLimit(
+        model=model,
+        geom_pairs=collision_pairs,  # type: ignore
+        minimum_distance_from_collisions=0.05,
+        collision_detection_distance=0.1,
+    )
+
+    limits = [
+        mink.ConfigurationLimit(model),
+        collision_avoidance_limit,
+    ]
 
     com_mid = model.body("com_target").mocapid[0]
     feet_mid = [model.body(f"{foot}_target").mocapid[0] for foot in feet]
@@ -75,7 +94,7 @@ if __name__ == "__main__":
         mujoco.mjv_defaultFreeCamera(model, viewer.cam)
 
         # Initialize to the home keyframe.
-        configuration.update_from_keyframe("stand")
+        configuration.update_from_keyframe("teleop")
         posture_task.set_target_from_configuration(configuration)
         pelvis_orientation_task.set_target_from_configuration(configuration)
         torso_orientation_task.set_target_from_configuration(configuration)
@@ -93,9 +112,16 @@ if __name__ == "__main__":
                 foot_task.set_target(mink.SE3.from_mocap_id(data, feet_mid[i]))
                 hand_task.set_target(mink.SE3.from_mocap_id(data, hands_mid[i]))
 
-            vel = mink.solve_ik(configuration, tasks, rate.dt, solver, 1e-1)
+            vel = mink.solve_ik(
+                configuration, tasks, rate.dt, solver, 1e-1, limits=limits
+            )
             configuration.integrate_inplace(vel, rate.dt)
             mujoco.mj_camlight(model, data)
+
+            # Note the below are optional: they are used to visualize the output of the
+            # fromto sensor which is used by the collision avoidance constraint.
+            mujoco.mj_fwdPosition(model, data)
+            mujoco.mj_sensorPos(model, data)
 
             # Visualize at fixed FPS.
             viewer.sync()
